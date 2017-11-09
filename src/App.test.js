64d7522664d874bddc1474ae3fc6cffd92ace75a
Mauricio Saglietto <msaglietto@gmail.com>
2017-11-09 19:00:40 +0100

    Initialize app with create-react-app, standard and enzyme

---
+++ src/App.test.js
@@ -0,0 +1,10 @@
+/* eslint-env jest */
+import React from 'react'
+import { shallow } from 'enzyme'
+import App from './App'
+
+it('renders without crashing', () => {
+  const wrapper = shallow(<App />)
+
+  expect(wrapper).toMatchSnapshot()
+})
